17c95585d809699b7c5804c28d293568a3870c36
chrisebrell <cls2003tkd@gmail.com>
2023-10-28 15:38:26 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -39,7 +39,26 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id')
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        if(!req.body.pic) {
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
 })
 
 router.get('/:id/edit', (req, res) => {
@@ -51,7 +70,7 @@ router.get('/:id/edit', (req, res) => {
         res.render('error404')
         }
         else {
-            res.render('places/edit', {place: places[id]})
+            res.render('places/edit', {place: places[id], id})
         }
     })
 
