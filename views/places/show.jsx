055404fb4ed9f2f4a4d486b23c01fe07da9472f7
chrisebrell <cls2003tkd@gmail.com>
2023-11-11 11:37:16 -0500

    Added average star rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show (data) {
             No comments yet! Stay tuned!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings/data.place.comments.length 
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -34,7 +48,7 @@ function show (data) {
                 </div>
                 <div className ="col-sm-6">
                 <h2>Rating</h2>
-                <p>currently unrated</p>
+                {rating}
                 <h2>Description</h2>
                 <h3>
                     {data.place.showEstablished()}
